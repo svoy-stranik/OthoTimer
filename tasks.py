ce0bf0bd9e45e69d0eda57adb41995c684a71ab1
v01d <v01d@v01d.ru>
2025-11-18 20:51:00 +0400

    Init (#1)

---
+++ tasks.py
@@ -0,0 +1,45 @@
+from os import system
+from pathlib import Path
+from shutil import rmtree
+
+from invoke.context import Context
+from invoke.tasks import task
+
+
+@task(default=True)
+def run(_: Context):
+    system("uv run python src/main.py")
+
+
+@task
+def lint(_: Context):
+    system("uv run ruff check")
+
+
+@task
+def format(_: Context):
+    system("uv run ruff format")
+
+
+@task(pre=[format])
+def lintfix(_: Context):
+    system("uv run ruff check --fix")
+
+
+@task
+def build(_: Context):
+    rmtree("dist", ignore_errors=True)
+
+    # TODO: Add -i assets/icon.ico
+
+    system(
+        """
+            uv run pyinstaller \
+                -Fwn OthoTimer \
+                -p src \
+                src/main.py
+        """
+    )
+
+    Path("OthoTimer.spec").unlink()
+    rmtree("build", ignore_errors=True)
